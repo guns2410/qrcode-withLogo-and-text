fd1abbcac54bb0c2282065773b7b70986b7d0622
Gunjan Soni <gunjan.soni2002@gmail.com>
2018-05-26 13:22:43 +0530

    source code

---
+++ index.js
@@ -0,0 +1,34 @@
+const QRCode = require('qrcode');
+const images = require('images');
+const Jimp = require('jimp');
+
+const LogoFileName = './assets/logo.png';
+const QRCodeFileName = './qr.png';
+const text = '1234567890';
+const TextOutPutFileName = `./outputs/${text}.png`;
+const OutputFileName = `./outputs/${text}-temp.png`;
+const QRCodeWidth = 512;
+const LogoLocation = (QRCodeWidth / 2) - 50;
+
+QRCode.toFile(QRCodeFileName, text, {
+    width: QRCodeWidth,
+    errorCorrectionLevel: 'H',
+}, (err) => {
+    if (err) console.error(err.message, err);
+
+    images(QRCodeFileName)
+        .draw(images(LogoFileName), LogoLocation, LogoLocation)
+        .save(OutputFileName, { quality: 100 });
+
+    let loadedImage;
+    Jimp.read(OutputFileName)
+    .then((image) => {
+        loadedImage = image;
+        return Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
+    })
+    .then(font => loadedImage.print(font, 10, QRCodeWidth - 42, text)
+                   .write(TextOutPutFileName))
+    .catch((err) => {
+        console.error(err);
+    });
+});
